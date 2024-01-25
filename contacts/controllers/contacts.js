bce456f13f22b91ce1369c3a9d7b596c05480407
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2024-01-24 20:10:54 -0600

    contacts controller crud

--- contacts/controllers/contacts.js
+++ contacts/controllers/contacts.js
@@ -1,17 +1,36 @@
 /* contacts controller */
-import { dbConnect, getDb } from "../db/connection.js";
-import { ObjectId, BSON } from "mongodb";
+import { contactsCollection } from "../db/connection.js";
+import { BSON } from "mongodb";
 import 'dotenv/config';
 
 
+//auxilliary
+function buildIdQuery(idNum) {
+    const bId = new BSON.ObjectId(req.params.contact_id)
+    return { _id: bId }
+}
+
+/* --create-- */
+export const addContact = async (req,res,next) => {
+    try {
+        const result = await contactsCollection().insertOne(req.body);
+        console.log(`New contact added with _id: ${result.insertedId}`);
+        res.redirect('/contacts');
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+/* --read-- */
 // get all documents form the contacts table
 export const getAllContacts = async (req,res,next) => {
     //connect &query database
-    const result = await getDb().db(process.env.DB_NAME).collection("contacts").find({});
+    const result = await contactsCollection().find({});
 
     //output result
     if (result) {
-        console.log(`Found the following contact(s): `);
+        //console.log(`Found the following contact(s): `);
         const results = await result.toArray();
         res.send(results);
     } else {
@@ -26,7 +45,7 @@ export const getContactById = async (req,res,next) => {
     const contactToGet = { _id: bId }
 
     //query
-    const result = await getDb().db(process.env.DB_NAME).collection("contacts").findOne(contactToGet);
+    const result = await contactsCollection().findOne(contactToGet);
 
     if(result) {
         console.log("Found a contact: ");
@@ -35,5 +54,54 @@ export const getContactById = async (req,res,next) => {
     } else {
         res.send("No contacts found :(")
     }
-    
+}
+
+/* --update-- */
+
+export const updateContactbyId = async (req,res,next) => {
+    try {
+        //build query
+        const contactToFind = buildIdQuery(req.body.contact_id)
+        //build update
+        const update = {
+            $set: {
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email,
+                favoriteColor: req.body.favoriteColor,
+                birthday: req.body.birthday
+            }
+        }
+        const options = req.body.options;
+        //send it
+        const result = await contactsCollection().findOneAndUpdate(contactToFind, update, options);
+        if (result) {
+            console.log(`Contact updated with _id: ${result.value._id}`);
+            res.json('Success!');
+        } else {
+            console.log(`Something went wrong :[`);
+            res.json('Something went wrong');
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+/* --delete-- */
+export const deleteContactById = async (req,res,next) => {
+    try { 
+        const contactToDelete = buildIdQuery(req.body.contact_id);
+
+        const result = await contactsCollection().deleteOne(contactToDelete)
+        if (result.deletedCount === 0) {
+            console.log(`Something went wrong :[`);
+            res.json('Something went wrong');
+            
+        } else {
+            res.json('Success!');
+        }
+    } catch (error) {
+        console.error(error);
+    }
 }
