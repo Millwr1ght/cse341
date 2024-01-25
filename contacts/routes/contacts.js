bd1e664d9cb26d0e15b65b6337a0a29d478762cb
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2024-01-24 19:59:50 -0600

    contacts: create, update, delete routes

--- contacts/routes/contacts.js
+++ contacts/routes/contacts.js
@@ -4,7 +4,20 @@ import * as contactsController from '../controllers/contacts.js';
 //contacts router
 export const contacts_routes = Router();
 
+/* --create-- */
+
+//add contact to contacts table
+contacts_routes.post('/add', contactsController.addContact)
+
+/* --read-- */
+
 //get all contacts
 contacts_routes.get('/', contactsController.getAllContacts);
 
 contacts_routes.get('/:contact_id', contactsController.getContactById);
+
+/* --update-- */
+contacts_routes.put('/update', contactsController.updateContactbyId)
+
+/* --delete-- */
+contacts_routes.delete('/delete', contactsController.deleteContactById)
