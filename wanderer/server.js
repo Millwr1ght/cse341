8f1784e79073f6ac385ecb40b844a26f415c2fc1
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2024-03-16 21:28:54 -0500

    swagger autogen

--- wanderer/server.js
+++ wanderer/server.js
@@ -4,3 +4,29 @@ import routes from "./routes/index.js";
 import { dbConnect } from "./db/connection.js";
 import middlewareWrapper from "cors";
 import bodyParser from "body-parser";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app
+    .use(middlewareWrapper())
+    .use(bodyParser.json())
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        next();
+    })
+    .use('/', routes);
+
+
+
+dbConnect((err, mongodb) => {
+    if (err) {
+        console.log(err)
+    } else {
+        app.listen(port, () => {
+            console.log(`[server]: App listening on port ${port}`);
+        })
+    }
+})
