843650770d3d379e50ae2dbb237ae4e9ebb81fde
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2024-04-10 22:46:43 -0500

    commit the testing stuff

---
+++ wanderer/tests/users.test.js
@@ -0,0 +1,32 @@
+import { jest } from '@jest/globals';
+import { MongoClient } from 'mongodb';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+describe('insert', () => {
+    let connection;
+    let db;
+
+    beforeAll(async () => {
+        connection = await MongoClient.connect(process.env.DB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        db = await connection.db(process.env.DB_NAME);
+    });
+
+    afterAll(async () => {
+        await connection.close();
+    });
+
+    it('should insert a doc into collection', async () => {
+        const users = db.collection('users');
+
+        const mockUser = { _id: 'some-user-id', username: 'John' };
+        await users.insertOne(mockUser);
+
+        const insertedUser = await users.findOne({ _id: 'some-user-id' });
+        expect(insertedUser).toEqual(mockUser);
+    });
+});
